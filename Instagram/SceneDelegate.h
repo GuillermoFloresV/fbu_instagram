d0f29aa7aa825afea3a74ac5f34f63d32734899a
Guillermo Flores V <gfloresv@fb.com>
2020-07-06 15:23:13 -0700

    Initial Commit

---
+++ Instagram/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Instagram
+//
+//  Created by gfloresv on 7/6/20.
+//  Copyright © 2020 gfloresv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by gfloresv on 7/6/20.
+//  Copyright © 2020 gfloresv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
