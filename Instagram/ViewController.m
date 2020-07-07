2fcd30b1047ca39bc2f679009fbdb3267045a8c3
Guillermo Flores V <gfloresv@fb.com>
2020-07-06 22:16:11 -0700

    User can now sign up to the parse server

--- Instagram/ViewController.m
+++ Instagram/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import <Parse/Parse.h>
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -18,6 +20,43 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)loginAction:(id)sender {
+    
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+//    if(username.length ==0 || password.length ==0)
+//    {
+//        
+//    }
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+        }
+    }];
+}
 
+- (IBAction)signUpAction:(id)sender {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+}
 
 @end
