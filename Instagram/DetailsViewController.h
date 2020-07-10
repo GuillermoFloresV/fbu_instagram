ebe4e6f7914f90d5221258092092dafb29cc8172
Guillermo Flores V <gfloresv@fb.com>
2020-07-10 15:35:04 -0700

    added details view controller

---
+++ Instagram/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  Instagram
+//
+//  Created by gfloresv on 7/10/20.
+//  Copyright © 2020 gfloresv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/DetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailsViewController.m
+//  Instagram
+//
+//  Created by gfloresv on 7/10/20.
+//  Copyright © 2020 gfloresv. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
